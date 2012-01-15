9d519c0298fe3a471a0d33f0f164c956b94632c2
Philip Proplesch <philip@proplesch.de>
2012-01-15 15:38:23 +0100

    Added StringExtensions

---
+++ src/XmlRpcMvc/Extensions/StringExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace XmlRpcMvc.Extensions
+{
+    public static class StringExtensions
+    {
+        public static DateTime ConvertToDateTime(this string instance)
+        {
+            DateTime dateTime;
+
+            if (DateTime.TryParse(instance, out dateTime))
+                return dateTime;
+
+            return
+                new List<string>
+                    {
+                        "yyyyMMddTHH:mm:ss",
+                        "yyyyMMddTHH:mm:ssZ"
+                    }
+                    .Any(
+                        format =>
+                        DateTime.TryParseExact(
+                            instance,
+                            format,
+                            CultureInfo.InvariantCulture,
+                            DateTimeStyles.None,
+                            out dateTime))
+                    ? dateTime
+                    : DateTime.MinValue;
+        }
+    }
+}
